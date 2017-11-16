0b1201bb81fb02d514c5e6ec92ed7b1046bd1086
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-11-16 15:49:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,15 +2,9 @@
 # out the current state.
 
 def display_board(board)
-
-  rows = []
-
-    rows[0] " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
-
-board = [" "," "," "," "," "," "," "," "," "]
-print display_board(board)
